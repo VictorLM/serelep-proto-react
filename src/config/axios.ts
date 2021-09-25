31731dec90e5e66d919c8e3f2c7edd4cc901ee81
Victor Meireles <victordinami@gmail.com>
2021-09-25 20:02:54 -0300

    Correção axios API URL

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 let baseURL = '';
 
 if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://serelep-proto.herokuapp.com';
+  baseURL = 'https://api.avincer.co';
 } else if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3001';
 }
