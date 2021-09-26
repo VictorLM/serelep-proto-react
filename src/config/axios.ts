fca6955e9b4d1e76e528b105cbbed7653d4a76d8
Victor Meireles <victordinami@gmail.com>
2021-09-26 17:20:24 -0300

    API URL heroku

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 let baseURL = '';
 
 if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://api.avincer.co';
+  baseURL = 'https://avincer-api.herokuapp.com';
 } else if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3001';
 }
